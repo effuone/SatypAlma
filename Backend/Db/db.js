560ca0ffec5c14d31a0b6c21844564d658580914
effuone <effuone>
2022-07-20 21:23:06 +0600

    switched to hosted db

--- Backend/Db/db.js
+++ Backend/Db/db.js
@@ -1,13 +1,13 @@
 import {Pool} from "postgres-pool"
 import 'dotenv/config'
 const pgPool = new Pool({
-    user: 'satypalmauser',
-    host:'localhost',
-    database: 'satypalmadb',
+    user: 'kianoiiopkfhtu',
+    host:'ec2-3-248-121-12.eu-west-1.compute.amazonaws.com',
+    database: 'dapr1ft4kuusn2',
     port: 5432,
-    password: 'satypalmauser123'
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
+    password: '966a12c3a097391c5c121145d963df09542469e540ab3205ed2511b3cee73ee3',
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 export default pgPool
