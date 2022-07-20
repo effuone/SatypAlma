16898c2b0afde02495287ae4095a055923346b11
effuone <effuone>
2022-07-21 01:30:07 +0600

    cors fix

--- Backend/index.js
+++ Backend/index.js
@@ -168,8 +168,17 @@ const updateDatabaseCategories = async ()=>{
     }
 }
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 const app = express()
 app.use(express.json())
+app.use(allowCrossDomain)
 app.use('/test', async (req,res)=>{
     res.send('Working')
 })
