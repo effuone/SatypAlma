c2cbc002fd1eb3101c59e6c5325ba5867561eff7
effuone <effuone>
2022-07-21 01:34:29 +0600

    cors policy add

--- Backend/index.js
+++ Backend/index.js
@@ -169,7 +169,7 @@ const updateDatabaseCategories = async ()=>{
 }
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
